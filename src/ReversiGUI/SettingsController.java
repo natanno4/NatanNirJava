787c5724b5f5348c4b6e4cd26cbf9aa2a39e89ff
niraz888 <33843980+niraz888@users.noreply.github.com>
2018-01-15 20:35:22 +0200

    update SettingsController

--- src/ReversiGUI/SettingsController.java
+++ src/ReversiGUI/SettingsController.java
@@ -1,5 +1,7 @@
 package ReversiGUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,12 +9,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-
+import javafx.event.Event;
 import javax.print.attribute.standard.OutputDeviceAssigned;
+
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,44 +26,61 @@ import java.util.List;
 import java.awt.event.ActionEvent;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Observable;
 
 public class SettingsController {
     @FXML
     private Button save;
     @FXML
-    private TextField starter;
+    private ChoiceBox starter;
     @FXML
-    private TextField colorOfPlayerO;
+    private ColorPicker colorOfPlayerO;
     @FXML
-    private TextField colorOfPlayerX;
+    private ColorPicker colorOfPlayerX;
     @FXML
-    private TextField boardSize;
+    private ChoiceBox boardSize;
 
-    private List<String> list = Arrays.asList("blue","green","red","gray", "pink","yellow",
-            "black", "white");
+    private ObservableList<Integer> options = FXCollections.observableArrayList(4, 6, 8
+    , 10, 12, 14 , 16, 18, 20);
 
     @FXML
-    protected void saveSettingsToFile(ActionEvent event) {
+    protected void initialize() {
+        this.boardSize.setItems(options);
+        this.starter.setItems(FXCollections.observableArrayList("X", "O"));
+    }
+
+
+    @FXML
+    protected void saveSettingsToFile(Event event) {
+
         try {
-            if (this.checkValid()) {
-                String Xcolor = this.colorOfPlayerX.getText();
-                String Ocolor = this.colorOfPlayerO.getText();
-                Integer size = Integer.parseInt(this.boardSize.getText());
-                String whoStart = this.starter.toString();
 
-                OutputStreamWriter is = null;
+            if (this.checkValid()) {
+
+                Color Xcolor = this.colorOfPlayerX.getValue();
+                String s = Xcolor.toString();
+                System.out.println(s + "natan ugosheniaaaaaaaaaaaa");
+                String Ocolor = this.colorOfPlayerO.getValue().toString();
+                String size = this.boardSize.getValue().toString();
+
+                String whoStart = this.starter.getValue().toString();
+
+                OutputStreamWriter os = null;
                 try {
-                    is = new OutputStreamWriter(new FileOutputStream("settings.txt"));
-                    is.write(Xcolor);
-                    is.write(Ocolor);
-                    is.write(size);
-                    is.write(whoStart);
+                    os = new OutputStreamWriter(new FileOutputStream(new File(("src/settings.txt"))));
+                    os.write(size);
+                    os.write("\n");
+                    os.write(s);
+                    os.write("\n");
+                    os.write(Ocolor);
+                    os.write("\n");
+                    os.write(whoStart);
                 } catch (Exception e) {
                     System.out.println("error");
                 } finally {
                     try {
-                        if (is != null) {
-                            is.close();
+                        if (os != null) {
+                            os.close();
                         }
                     } catch (IOException e) {
                         System.out.println("error in closing file");
@@ -79,17 +102,14 @@ public class SettingsController {
         }
     }
     private boolean checkValid() {
-
+        /*
         String colorX = this.colorOfPlayerX.getText();
         String colorO = this.colorOfPlayerO.getText();
-
         String size = this.boardSize.getText();
-
         String start = this.starter.getText();
         String start2 = start.toUpperCase();
-
         Integer sizeB = Integer.parseInt(size);
-        if (!start2.equals("X") || !start2.equals("O")) {
+        if (!start2.equals("X") && !start2.equals("O")) {
             return false;
         }
         if (sizeB > 20 || sizeB < 4) {
@@ -98,7 +118,8 @@ public class SettingsController {
         if (!this.list.contains(colorX) || !this.list.contains(colorO)) {
             return false;
         }
-
+            */
         return true;
     }
+
 }
